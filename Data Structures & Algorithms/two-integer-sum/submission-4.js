e09c989d4ae480b3290e9d2403f88a75ce26e055
Deepa Ashok Kumar <56334707+deepakumar19@users.noreply.github.com>
2026-05-24 12:59:04 +0530

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        //Brute Force
+        // let result = [];
+        // for (let i = 0; i < nums.length; i++) {
+        //     for (let j = i+1; j < nums.length; j++) {
+        //         if (nums[i] + nums[j] === target) {
+        //             result.push(i, j);
+        //         }
+        //     }
+        // }
+        // return result;
+
+        let map = new Map();
+        for(let i=0;i<nums.length;i++){
+            const rem = target - nums[i];
+            if(map.has(rem)){
+                return [map.get(rem),i];
+            }
+            map.set(nums[i],i);
+        }
+
+    }
+
+
+}
